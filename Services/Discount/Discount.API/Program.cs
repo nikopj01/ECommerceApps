4c9aeda5c91b6d30cdbb41fe8530c46239c0ceda
Niko <nikopj01@gmail.com>
2023-09-24 20:32:28 +0900

    Added PostgreSQL to Discount.API

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,12 +1,29 @@
+using Discount.API.Repositories;
+using Discount.API.Repositories.Interfaces;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
+}
 
 app.UseHttpsRedirection();
 
